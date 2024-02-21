e37773f6b68541f1c0cb9c4a4eddef91a3eab75b
wwwhyuk <joohyeok5118@naver.com>
2024-02-21 11:15:43 +0900

    StringBulider

--- test/String/stringTest.cs
+++ test/String/stringTest.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharpStudy.String
+﻿using System.Text;
+
+namespace CSharpStudy.String
 {
     class stringTest
     {
@@ -24,12 +26,29 @@
             s3 = s2.Substring(0, 2); // 문자열 추출 
             Console.WriteLine(s3 + "...");
                         
-            c1 = s2.ToCharArray(); // 묹자열을 문자배열로 출력
+            c1 = s2.ToCharArray(); // 묹자열을 문자배열로 변환
 
             Console.WriteLine(c1);
 
-            foreach(var items in c2)
+            foreach(var items in c1)
                 Console.WriteLine(items);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i <26; i++)
+            {
+                sb.Append(i.ToString());
+                //sb.Append(System.Environment.NewLine);
+            }
+            string s = sb.ToString();
+            Console.WriteLine(sb); // s를 뽑으나 sb를 뽑으나 같다.
+            /*
+             StringBuilder의 효율성: StringBuilder는 문자열을 
+            효율적으로 처리하기 위한 클래스입니다. 문자열을 조작할
+            때마다 새로운 문자열 객체가 생성되는 것이 아니라 내부 버퍼에
+            문자열을 추가하고, 이를 효율적으로 관리합니다. 
+            따라서 문자열을 조작하는 경우에는 StringBuilder를 
+            사용하는 것이 성능상 이점이 있다.
+             */
         }
     }
 }
